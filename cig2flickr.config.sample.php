947bc59f9a5b2cb637f5ab8b5bea646f6cbc8419
Gregoire Pailler <gregoire@pailler.fr>
2013-06-23 14:29:20 +0200

    Change comments in configuration file

--- cig2flickr.config.sample.php
+++ cig2flickr.config.sample.php
@@ -4,15 +4,14 @@
 define('CIG_USERNAME', 'YOUR_CIG_USERNAME');
 define('CIG_PASSWORD', 'YOUR_CIG_PASSWORD');
 
-// You can get your key and secret by creating an application
-// on http://www.flickr.com/services/apps/create/apply/
+// Flickr API key and secret
+// http://www.flickr.com/services/apps/create/apply/
 define('FLICKR_API_KEY', 'YOUR_API_KEY');
 define('FLICKR_API_SECRET', 'YOUR_API_SECRET');
 
-// Call the script from your browser and validate Flickr authorization,
-// then you can obtain access tokens
+// Flickr authorization result tokens
 define('FLICKR_ACCESS_TOKEN', '');
 define('FLICKR_ACCESS_TOKEN_SECRET', '');
 
-// You can customize your host for Flickr callback 
+// Custom hostname for Flickr callback 
 // define('HOSTNAME', 'www.yourserver.com');
